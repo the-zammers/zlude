e4e14da07dddbbaa094b9330965ed6874107a8e4
Z Cater <zcater40@stuy.edu>
2024-08-19 19:53:30 +0000

    Fixed vectors and added additional instance definitions (it\'s a mess but oh well)

--- Data/NumbersTwo.hs
+++ Data/NumbersTwo.hs
@@ -5,6 +5,7 @@ module Data.NumbersTwo where
 
 import Data.Group
 import Data.Ring
+import Data.Tuple (fst, snd)
 import Data.Functor (Functor(..), (<$>))
 import Data.Foldable (Foldable(..))
 import Data.Traversable (Traversable(..), sequenceA)
@@ -13,31 +14,81 @@ import Data.Function (($))
 import GHC.Base (Eq)
 import Text.Show (Show)
 
-class (Applicative v, Traversable v) => FixedVector v
+unzip :: Functor f => f (a,b) -> (f a, f b)
+unzip xs = (fst <$> xs, snd <$> xs)
 
 data Vec2 a = Vec2 a a deriving (Eq, Show, Functor, Foldable, Traversable)
 instance Applicative Vec2 where
   pure x = Vec2 x x
   (Vec2 a1 a2) <*> (Vec2 b1 b2) = Vec2 (a1 b1) (a2 b2)
-instance FixedVector Vec2
+
+instance Semigroup (Sum a) => Semigroup (Sum (Vec2 a)) where
+  v1 <> v2 = sequenceA ((<>) <$> sequenceA v1 <*> sequenceA v2)
+instance Monoid (Sum a) => Monoid (Sum (Vec2 a)) where
+  mempty = sequenceA (pure mempty)
+instance Group (Sum a) => Group (Sum (Vec2 a)) where
+  invert v = sequenceA $ invert <$> sequenceA v
+instance Abelian (Sum a) => Abelian (Sum (Vec2 a))
+instance Semigroup (Product a) => Semigroup (Product (Vec2 a)) where
+  v1 <> v2 = sequenceA ((<>) <$> sequenceA v1 <*> sequenceA v2)
+instance Monoid (Product a) => Monoid (Product (Vec2 a)) where
+  mempty = sequenceA (pure mempty)
+instance Group (Product a) => Group (Product (Vec2 a)) where
+  invert v = sequenceA $ invert <$> sequenceA v
+instance Abelian (Product a) => Abelian (Product (Vec2 a))
+instance Semiring a => Semiring (Vec2 a) where
+  v1 + v2 = (+) <$> v1 <*> v2
+  v1 * v2 = (*) <$> v1 <*> v2
+  zero = pure zero
+  one = pure one
+instance Ring a => Ring (Vec2 a) where
+  v1 - v2 = (-) <$> v1 <*> v2
+  negate = fmap negate
+  abs = fmap abs
+  signum = fmap abs
+instance DivisionRing a => DivisionRing (Vec2 a) where
+  fromRational x = pure (fromRational x)
+  v1 / v2 = (/) <$> v1 <*> v2
+  recip = fmap recip
+instance CommutativeRing a => CommutativeRing (Vec2 a)
+instance EuclideanDomain a => EuclideanDomain (Vec2 a) where
+  v0 `quotRem` v1 = unzip $ quotRem <$> v0 <*> v1
+instance Field a => Field (Vec2 a)
+
 
 data Vec3 a = Vec3 a a a deriving (Eq, Show, Functor, Foldable, Traversable)
 instance Applicative Vec3 where
   pure x = Vec3 x x x
   (Vec3 a1 a2 a3) <*> (Vec3 b1 b2 b3) = Vec3 (a1 b1) (a2 b2) (a3 b3)
-instance FixedVector Vec3
-
-instance (FixedVector v, Semigroup (Sum a)) => Semigroup (Sum (v a)) where
+instance Semigroup (Sum a) => Semigroup (Sum (Vec3 a)) where
   v1 <> v2 = sequenceA ((<>) <$> sequenceA v1 <*> sequenceA v2)
-instance (FixedVector v, Monoid (Sum a)) => Monoid (Sum (v a)) where
+instance Monoid (Sum a) => Monoid (Sum (Vec3 a)) where
   mempty = sequenceA (pure mempty)
-instance (FixedVector v, Abelian (Sum a)) => Abelian (Sum (v a))
-instance (FixedVector v, Semigroup (Product a)) => Semigroup (Product (v a)) where
+instance Group (Sum a) => Group (Sum (Vec3 a)) where
+  invert v = sequenceA $ invert <$> sequenceA v
+instance Abelian (Sum a) => Abelian (Sum (Vec3 a))
+instance Semigroup (Product a) => Semigroup (Product (Vec3 a)) where
   v1 <> v2 = sequenceA ((<>) <$> sequenceA v1 <*> sequenceA v2)
-instance (FixedVector v, Monoid (Product a)) => Monoid (Product (v a)) where
+instance Monoid (Product a) => Monoid (Product (Vec3 a)) where
   mempty = sequenceA (pure mempty)
-instance (FixedVector v, Semiring a) => Semiring (v a) where
+instance Group (Product a) => Group (Product (Vec3 a)) where
+  invert v = sequenceA $ invert <$> sequenceA v
+instance Abelian (Product a) => Abelian (Product (Vec3 a))
+instance Semiring a => Semiring (Vec3 a) where
   v1 + v2 = (+) <$> v1 <*> v2
   v1 * v2 = (*) <$> v1 <*> v2
   zero = pure zero
   one = pure one
+instance Ring a => Ring (Vec3 a) where
+  v1 - v2 = (-) <$> v1 <*> v2
+  negate = fmap negate
+  abs = fmap abs
+  signum = fmap abs
+instance DivisionRing a => DivisionRing (Vec3 a) where
+  fromRational x = pure (fromRational x)
+  v1 / v2 = (/) <$> v1 <*> v2
+  recip = fmap recip
+instance CommutativeRing a => CommutativeRing (Vec3 a)
+instance EuclideanDomain a => EuclideanDomain (Vec3 a) where
+  v0 `quotRem` v1 = unzip $ quotRem <$> v0 <*> v1
+instance Field a => Field (Vec3 a)
--- Main.hs
+++ Main.hs
@@ -1,17 +1,35 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE CPP #-}
 
 module Main where
 
-import System.IO (IO, print)
+import System.IO (IO, print, putStr, putStrLn)
 import Control.Applicative (pure)
+import GHC.Base (Ordering(..))
 import Data.Group
 import Data.Ring
 import Data.Numbers
 import Data.NumbersTwo
 
+#define TEST(expr) putStr "expr\n> "; print (expr)
 
 main :: IO ()
 main = do
-  print ((5 :: Int) * 3 - 2)
-  print (Vec2 (12 :: Float) 84 + Vec2 3 2 * Vec2 18 7)
+  putStrLn "Data.Group examples: (need better demo of Group class)"
+  TEST ("hello, " <> "world!")
+  TEST (EQ <> EQ <> GT <> EQ <> LT)
+  TEST ("gree" <> mempty <> "tings")
+  TEST (((), mempty) <> (mempty, ()))
+  TEST (invert () <> ())
+  putStrLn ""
+
+  putStrLn "Data.Ring / Data.Numbers (reimplementations of existing number types) examples:"
+  TEST ((5 :: Int) * 3 - 2)
+  TEST (18 + (2 :: Rational) / 2 + 10)
+  TEST ((1 + 2 + (3 :: Double)) * 100 / 25)
+  putStrLn ""
+
+  putStrLn "Data.NumbersTwo (new number types) examples:"
+  TEST (Vec2 (12 :: Integer) 100 + Vec2 3 2 * (Vec2 18 7 `div` Vec2 6 6))
+  TEST ((Vec3 0 (0 :: Float) 0 - Vec3 99 99 99) / Vec3 3 9 11)
 
